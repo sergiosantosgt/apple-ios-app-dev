950e27858551577ca58dd923ae4b4ccce125e556
Sergio Santos <sergiosantos@MacBook-Air-de-Sergio.local>
2024-05-05 00:12:29 -0300

    add first app

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sergio Santos on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
